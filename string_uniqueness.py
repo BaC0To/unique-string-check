56a19d9feb75ee1655f53095a8db7706d9135dd8
BaC0To <vasil.v.kirov@gmail.com>
2023-09-12 15:05:45 +0300

    function simplified

--- string_uniqueness.py
+++ string_uniqueness.py
@@ -1,31 +1,9 @@
-class UniqueString:
+def is_unique(input_string) -> bool:
+    """Function that checks if all characters in a string are unique
+    params: input_string: string
+    return: bool"""
 
+    return len(input_string) == len(set(input_string))
+    
 
-    @staticmethod
-    def unique(input_string):
-        """Function that checks if all characters in a string are unique
-        params: input_string : str
-        return: isUnique: bool"""
-
-        try:        
-            isUnique = False
-            
-            input_string_length = len(input_string)
-            input_string_to_set = set(input_string)
-            length_input_string_to_set = len(input_string_to_set)
-
-            input_string_to_list = list(input_string)
-            length_input_string_to_list = len (input_string_to_list)
-
-            if length_input_string_to_set == length_input_string_to_list:
-                isUnique = True
-
-            return isUnique
-        
-        except (TypeError) as exc:
-            print(f"Error: {exc} ; Function only input must be a string!")
-        
-
-""" test_string1 = UniqueString()
-current_result = test_string1.unique("abbc")
-print(f"String_Uniqieness_result is: {current_result}") """
+print(is_unique("abc"))
--- test_string_uniqueness.py
+++ test_string_uniqueness.py
@@ -1,32 +1,18 @@
 import unittest
 
-from string_uniqueness import UniqueString
-
+from string_uniqueness import is_unique
 
 class TestUniqueString(unittest.TestCase):
 
-    def setUp(self) -> None:
-        self.string_to_be_tested = UniqueString()
 
-    def tearDown(self) -> None:
-        del self.string_to_be_tested
+    def test_if_string_unique(self):
+        unique_string = "qwerty"
+        self.assertTrue(is_unique(unique_string))
 
-    def test_string_is_unique(self):
-        """
-        Test whether a string is a unique or not --> True/False
-        """
-        dummy_string = "abcd"
-        result = self.string_to_be_tested.unique(dummy_string)
-        self.assertTrue(result)
-
-    def test_string_is_not_unique(self):
-        """
-        Test whether a string is a unique or not --> True/False
-        """
-        dummy_string = "abcdabcd"
-        result = self.string_to_be_tested.unique(dummy_string)
-        self.assertFalse(result)
-
+    def test_if_string_not_unique(self):
+        not_unique_string = "qwertyqwerty"
+        self.assertFalse(is_unique(not_unique_string))
+    
 
 if __name__ == '__main__':
     unittest.main()
